0b8518b2d73f712ce58eee7653faa98213894aeb
James-Earley <james.albert.earley@gmail.com>
2022-12-26 17:56:22 +0000

    Create number_guess.sql

---
+++ number_guess.sql
@@ -0,0 +1,71 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 12.9 (Ubuntu 12.9-2.pgdg20.04+1)
+-- Dumped by pg_dump version 12.9 (Ubuntu 12.9-2.pgdg20.04+1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+DROP DATABASE number_guess;
+--
+-- Name: number_guess; Type: DATABASE; Schema: -; Owner: freecodecamp
+--
+
+CREATE DATABASE number_guess WITH TEMPLATE = template0 ENCODING = 'UTF8' LC_COLLATE = 'C.UTF-8' LC_CTYPE = 'C.UTF-8';
+
+
+ALTER DATABASE number_guess OWNER TO freecodecamp;
+
+\connect number_guess
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: game_details; Type: TABLE; Schema: public; Owner: freecodecamp
+--
+
+CREATE TABLE public.game_details (
+    username character varying(22),
+    count integer,
+    number integer
+);
+
+
+ALTER TABLE public.game_details OWNER TO freecodecamp;
+
+--
+-- Data for Name: game_details; Type: TABLE DATA; Schema: public; Owner: freecodecamp
+--
+
+INSERT INTO public.game_details VALUES ('Alby', NULL, NULL);
+INSERT INTO public.game_details VALUES ('user_1671633944735', NULL, NULL);
+INSERT INTO public.game_details VALUES ('user_1671633944734', NULL, NULL);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
